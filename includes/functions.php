765861a4cb59d50237cb2f401b649ec2ff15d8ba
ryan <ryan@ryanhightower.com>
2012-12-06 22:20:12 -0700

    Updated to current working version.

--- includes/functions.php
+++ includes/functions.php
@@ -22,3 +22,19 @@ function output_message($message=""){
 		return "";
 	}
 }
+
+function __autoload($class_name){
+	$class_name = strtolower($class_name);
+	$path = "../includes/{$class_name}.php";
+	if(file_exists($path)){
+		require_once($path);
+	} else {
+		die("The file {$class_name}.php could not be found.");
+	}
+}
+
+function include_layout_template($template=""){
+	include(SITE_ROOT.DS.'public'.DS.'layouts'.DS.$template);
+}
+
+
--- public/index.php
+++ public/index.php
@@ -1,2 +1,25 @@
 <?php
-echo "This works"; 
+
+require_once("../includes/initialize.php");
+
+?>
+
+<?php include_layout_template('header.php'); ?>
+
+<h1>Welcome to the Photo Gallery!</h1>
+
+<?php
+$user = User::find_by_id(1);
+echo $user->full_name();
+
+echo "<hr>";
+
+$users = User::find_all();
+foreach($users as $user){
+	echo "Username: ".$user->username."<br />";
+	echo "Password: ".$user->password."<br />";
+	echo "Name: ".$user->full_name()."<br />";
+}
+?>
+
+<?php include_layout_template('footer.php'); ?>
